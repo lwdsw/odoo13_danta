d03d7d119eff70a5c1a102bdff0fb0417af0a744
lwdsw <442511683@qq.com>
2020-01-08 19:35:40 +0800

    新增 看板视图

--- myaddons/zerone_books/models/zerone_book.py
+++ myaddons/zerone_books/models/zerone_book.py
@@ -1,11 +1,30 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
+
 class ZeroneBook(models.Model):
     _name = "zerone.book"
+    _inherit = ['image.mixin']
     _description = "Zerone Books"
 
     image_1920 = fields.Image(string="图书样例")
+
+    # image_1920 = fields.Image("图书样例", compute='_compute_image', compute_sudo=True)
+    # image_1024 = fields.Image("Image 1024", compute='_compute_image', compute_sudo=True)
+    # image_512 = fields.Image("Image 512", compute='_compute_image', compute_sudo=True)
+    # image_256 = fields.Image("Image 256", compute='_compute_image', compute_sudo=True)
+    # image_128 = fields.Image("Image 128", compute='_compute_image', compute_sudo=True)
+    #
+    # def _compute_image(self):
+    #     for book in self:
+    #         # We have to be in sudo to have access to the images
+    #         book_id = self.sudo().env['zerone.book'].browse(book.id)
+    #         book.image_1920 = book_id.image_1920
+    #         book.image_1024 = book_id.image_1024
+    #         book.image_512 = book_id.image_512
+    #         book.image_256 = book_id.image_256
+    #         book.image_128 = book_id.image_128
+
     name = fields.Char(string="图书名称", required=True)
     code = fields.Char(string="图书编号", copy=False, help="管理编号，用于快速定位图书")
     isbn = fields.Char(string="ISBN", copy=False)
@@ -39,4 +58,10 @@ class ZeroneBook(models.Model):
             raise ValidationError("图书编号必须是唯一的！")
 
     def action_text(self):
-        pass
+        print("表单视图中的按钮")
+
+    def button_call_function1(self):
+        self.borrowed = False
+
+    def button_call_function2(self):
+        self.borrowed = True
